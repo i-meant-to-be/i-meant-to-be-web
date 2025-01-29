d73394fb86e39858a6e4812b719028b0521117cd
Shawn Kang <csm06063@naver.com>
2025-01-29 22:43:35 +0900

    feat: Added footer at the bottom of HomePage

---
+++ project/src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import { IoLogoGithub, IoLogoInstagram } from 'react-icons/io5';
+import GradientButton from '../pages/HomePage/components/TextButton';
+import {
+  GITHUB_PROFILE_URL,
+  GITHUB_REPO_URL,
+  INSTAGRAM_URL,
+} from '../util/urls';
+
+export default function Footer() {
+  return (
+    <footer className="pt-20">
+      <div className="flex flex-col space-y-3 items-start">
+        <div className="flex flex-row space-x-1">
+          <button onClick={() => window.open(INSTAGRAM_URL, '_blank')}>
+            <IoLogoGithub size={32} className="hover:text-slate-400" />
+          </button>
+          <button onClick={() => window.open(GITHUB_PROFILE_URL, '_blank')}>
+            <IoLogoInstagram size={32} className="hover:text-slate-400" />
+          </button>
+        </div>
+
+        <div className="flex flex-row font-light text-xs">
+          <p>이 웹 페이지는 </p>
+          <button
+            onClick={() => window.open(GITHUB_REPO_URL, '_blank')}
+            className="underline hover:text-slate-400"
+          >
+            <p>이 GitHub 저장소</p>
+          </button>
+          <p>에서 개발, 관리 및 배포되고 있습니다.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
