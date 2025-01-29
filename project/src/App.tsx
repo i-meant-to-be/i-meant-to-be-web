5d7853a6e516b9d8312fe5ec36e0adac70f485e7
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2025-01-29 15:48:14 +0900

    feat: 자기소개 페이지 구현 (#19)
    * chore: Set configures and environments for development
    
    * chore: Migrated to TailwindCSS 4.0
    
    * feat: Wrote introduce on HomePage
    
    * chore: Removed stylelint package and related files

--- project/src/App.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
- return (
-    <div className="min-h-screen w-screen flex justify-center items-center">
-      <div className="m-4 px-10 py-4 bg-gradient-to-r from-gd1/90 to-gd2/90 rounded-full shadow-2xl">
-        <h1 className="text-2xl text-onGd font-bold">{"imeanttobe <t>"}</h1>
-      </div>
-    </div>
-  )
-}
