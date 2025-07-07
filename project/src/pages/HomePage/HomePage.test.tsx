ceab3030148ab0277bc95e1ad741f16240e9cbcb
Shawn Kang <csm06063@naver.com>
2025-07-08 04:57:15 +0900

    test: Updated test code

--- project/src/pages/HomePage/HomePage.test.tsx
+++ project/src/pages/HomePage/HomePage.test.tsx
@@ -2,10 +2,18 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import HomePage from './HomePage';
 import '@testing-library/jest-dom';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
 describe('HomePage', () => {
   it('checks button content', async () => {
-    render(<HomePage />);
+    const router = createMemoryRouter([
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+    ]);
+
+    render(<RouterProvider router={router} />);
 
     expect(screen.getByRole('button', { name: '강시운' })).toBeInTheDocument();
     expect(
