88cc5a131a8c8de0d510f82e49ef3609b36061ee
Shawn Kang <csm06063@naver.com>
2025-07-08 04:47:58 +0900

    chore: Update dependencies

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
 import { defineConfig as defineVitestConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { analyzer } from 'vite-bundle-analyzer';
 
 const viteConfig = defineViteConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), analyzer()],
 });
 
 const vitestConfig = defineVitestConfig({
